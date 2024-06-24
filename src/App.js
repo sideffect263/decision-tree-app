777033018ceeb2483a74e564aafa4e6cbdee52c0
ariel biton <arielbiton03@gmail.com>
2024-06-24 17:21:44 +0300

    numerical data Note

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div>
       <h1>Decision Tree Model Trainer</h1>
+      <p>Note: At the moment, only numerical data can be analyzed. If you want to use categorical data, please adjust and normalize accordingly. Updates are soon to come.</p>
+
       <UploadAndSelect />
     </div>
   );
